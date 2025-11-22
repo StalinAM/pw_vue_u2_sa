806ddca5b6a51cabaaf8897cd75bb63f100bd867
Stalin <sta_vla411@hotmail.com>
2025-11-21 19:42:53 -0500

    Taller 13 Componentes vue

---
+++ src/components/Contador.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Universidad Central del Ecuador</h1>
+    <h2>Carrera de Computación</h2>
+  </div>
+</template>
+<script></script>
+<style></style>
--- src/views/ContadorView.vue
+++ src/views/ContadorView.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <ContadorView />
+    <Contador />
+    <Contador />
   </div>
 </template>
 
 <script>
-import ContadorView from './views/ContadorView.vue'
+import Contador from '../components/Contador.vue'
 export default {
   components: {
-    ContadorView
+    Contador
   }
 }
 </script>
